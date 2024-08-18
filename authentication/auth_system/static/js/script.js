a6d9bb742eacc3e7b332aad7a9b40a0b0a037b34
Sameh Jamal <samjamalcode@gmail.com>
2024-08-18 17:28:13 +0000

    Initialize DOM elements, create getTotal function and add events to html elements

---
+++ authentication/auth_system/static/js/script.js
@@ -0,0 +1,14 @@
+// Initialize DOM elements
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+// Initialize variables
+let mood = "create";
+let tmp;
