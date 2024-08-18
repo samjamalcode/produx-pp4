8e14bf9293ef382c0a4a527dbd1b03ab872963fe
Sameh Jamal <samjamalcode@gmail.com>
2024-08-18 08:12:49 +0000

    Add registration form and static assets (CSS, JS, images)

--- authentication/auth_system/urls.py
+++ authentication/auth_system/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, Register
 
 urlpatterns =[
-    path('home/', HomePage, name="home-page")
+    path('home/', HomePage, name="home-page"),
+    path('register/', Register, name="register-page" )
 ]
