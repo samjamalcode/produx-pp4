6bb260ab69d88af714c7a6ad283ac45934d6a819
Sameh Jamal <samjamalcode@gmail.com>
2024-08-18 13:46:37 +0000

    Update templates, views and create login form

--- authentication/auth_system/views.py
+++ authentication/auth_system/views.py
@@ -13,12 +13,21 @@ def Register(request):
         email  = request.POST.get('email')
         password  = request.POST.get('pass')
 
+        # Create and save the new user
         new_user = User.objects.create_user(name, email, password)
         new_user.first_name = fname
         new_user.last_name = lname
 
         new_user.save()
-        return redirect('login-page')
+        return redirect('login-page')  # Redirect to the login page after successful registration
+
+    else:
+        # If the request is GET, render the registration form
+        return render(request, 'auth_system/register.html', {})
 
 def Login(request):
+    if request.method == 'POST':  
+        name  = request.POST.get('uname')
+        password  = request.POST.get('pass')
+    
     return render(request, 'auth_system/login.html', {})
