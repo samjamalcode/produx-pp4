fe0298393a260ac392a8ae51a8423bb9d6c0e9fa
Sameh Jamal <samjamalcode@gmail.com>
2024-08-18 07:12:45 +0000

    Update templates, views, and settings; add register, index  page and create boilerplate HTML

--- authentication/auth_system/views.py
+++ authentication/auth_system/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def HomePage(request):
+    return render(request, 'auth_system/index.html', {})
+
+def Register(request):
+    pass
+
+def Login(request):
+    pass
