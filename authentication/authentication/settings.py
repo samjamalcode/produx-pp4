8b61dd1bd91c9931574513cc881fbf945cbaa404
Sameh Jamal <samjamalcode@gmail.com>
2024-09-15 08:02:23 +0000

    Update error pages and CSS styling; reset database settings

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
-        'USER': 'produx_db',
-        'PASSWORD': 'Produx_crud1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
