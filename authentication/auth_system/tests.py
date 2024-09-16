4683b59bc9a74cfad30ba8dc54c2b7eb713255b9
Sameh Jamal <samjamalcode@gmail.com>
2024-09-16 01:55:50 +0000

    Update tests, settings, and requirements; add README folder for images

--- authentication/auth_system/tests.py
+++ authentication/auth_system/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Create a user
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_homepage_status_code_logged_out(self):
+        # Test homepage status code when logged out
+        response = self.client.get(reverse('home-page'))
+        self.assertEqual(response.status_code, 302)  # Expecting a redirect to login page
+
+    def test_homepage_status_code_logged_in(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('home-page'))
+        self.assertEqual(response.status_code, 200)  # Should be 200 if logged in
+
+    def test_login_page_status_code(self):
+        response = self.client.get(reverse('login-page'))
+        self.assertEqual(response.status_code, 200) 
